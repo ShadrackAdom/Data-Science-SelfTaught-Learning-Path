2bba7eb05997e050d7747a060a211c72df31807f
Shadrack Adom <shadomdatasci@gmail.com>
2023-05-10 13:16:28 -0700

    Newton-Raphson

---
+++ OSSU/MIT-Intro-to-CS-Python/FingerExercise/newtonraphson.py
@@ -0,0 +1,13 @@
+#Newton-Rashpson for square root 
+#Find x such that x**2 - 24 is within epsilon of 0
+epsilon = 0.01
+k =24.0
+guess = k/2.0
+steps = 0
+
+while abs(guess*guess - k) >= epsilon:
+    steps+=1
+    guess = guess -(((guess ** 2)- k )/ (2* guess))
+    
+print("Square root of", k, "is about ", guess)
+print("took about", steps, "steps")
