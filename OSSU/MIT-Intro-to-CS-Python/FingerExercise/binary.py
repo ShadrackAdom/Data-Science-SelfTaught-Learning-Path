fdfaafa7849cfc8daf8e8ec127e4655b121d7d15
Shadrack Adom <shadomdatasci@gmail.com>
2023-05-12 10:38:58 -0700

    Function converts decimal to binary

--- OSSU/MIT-Intro-to-CS-Python/FingerExercise/binary.py
+++ OSSU/MIT-Intro-to-CS-Python/FingerExercise/binary.py
@@ -1,40 +1,16 @@
-# #Decimal to Binary  Fucnction
-
-# i = 1
-# binarray = []
-# while i <= 128:
-#     binarray.append(i)
-#     i += i
-
-# nbinarray = binarray[::-1]
-    
-# print(nbinarray)
-
-
-# num_check = 0
-# num = 75
-# i = 0
-
-
-# for i in len(nbinarray + 1):
-    
-# 	if num == 0:
-#         print("f")
-#     else:
-        
-
-
-        
-    
 def decimal_to_binary(n):
+    #check if number == 0
     if n == 0:
         return '0'
     
+
     binary = ''
     while n > 0:
+        #divide the number n by 2 and append the remainder (either 0 or 1)
         binary = str(n % 2) + binary
         n //= 2
     
     return binary
 
+#print result
 print(decimal_to_binary(75))
