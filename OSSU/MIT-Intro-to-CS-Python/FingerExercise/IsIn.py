2b13a51a8d43fda764c360ed189fff5276f2911f
Shadrack Adom <shadomdatasci@gmail.com>
2023-05-17 12:16:33 -0700

    Function compares two strings

--- OSSU/MIT-Intro-to-CS-Python/FingerExercise/IsIn.py
+++ OSSU/MIT-Intro-to-CS-Python/FingerExercise/IsIn.py
@@ -1,5 +1,13 @@
+
+'''
+Write a function isIn that accepts two strings as
+arguments and returns True if either string occurs anywhere in the other,
+and False otherwise. Hint: you might want to use the built-in str
+operation in.
+'''
 first_string = input("Enter your first string: ")
 second_string = input("Enter your second string: ")
+
 
 def IsIn(stringone, stringtwo):
     if stringone in stringtwo:
@@ -9,12 +17,3 @@ def IsIn(stringone, stringtwo):
 
 print(IsIn(first_string, second_string))
 
-
-
-def isIn2(string1, string2):
-    if string1 in string2 or string2 in string1:
-        return True
-    else:
-        return False
-
-print(isIn2(first_string, second_string))
